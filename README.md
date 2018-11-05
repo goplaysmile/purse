a0d3c89eeb1513248b295ebf045fa26ada79b1cd
Alex <a.matulionis@usa.com>
2018-11-04 17:50:18 -0800

    Added sprite directions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # purse
-Online RPG Engine 👜 ← ⚛️ 🔥🍃
+Serverless ORPG Engine 👜 ← ⚛️ 🔥🍃
+
+To try out preview:
+```npm
+git clone https://github.com/goplaysmile/purse.git
+
+cd purse
+
+npm start
+```
+(Make sure you have both _git_ and _npm_ installed)
+
+Features:
+* Try on any browser (mobile, desktop)
+* Change your sprite by clicking/tapping anywhere
+* Face elsewhere with arrow keys (no mobile yet)
--- src/components/App.js
+++ src/components/App.js
@@ -7,27 +7,57 @@ import DivIcon from './DivIcon'
 import spritesPng from '../assets/sprites/sprites0.png'
 import '../styles/App.css'
 
+console.log(`window.devicePixelRatio ${window.devicePixelRatio}`)
+
+const frames = {
+  'up':    [0,1,2,1],
+  'right': [9,10,11,10],
+  'down':  [3,4,5,4],
+  'left':  [6,7,8,7]
+}
+
+const keyToLR = {
+  'ArrowLeft': 'left',
+  'ArrowRight': 'right'
+}
+
+const keyToUD = {
+  'ArrowUp': 'up',
+  'ArrowDown': 'down'
+}
+
 class App extends Component {
-  constructor() {
-    super()
-
-    this.state = {}
+  state = {
+    xy: [0,0],
+    to: [null,null],
+    spr: 0,
   }
+
+  keyDown = e => {
+    this.setState(({ to:[ lr, ud ], dir }) => {
+      const newLR = keyToLR[e.key] || lr
+      const newUD = keyToUD[e.key] || ud
   
-  componentDidMount() {
-    // window.addEventListener('keydown', this.keyDown.bind(this), true)
-    // window.addEventListener('keyup', this.keyUp.bind(this), true)
+      if (newLR === lr && newUD === ud) return
 
-    if (!this.animFrameRef) {
-      this.animFrameRef = window.requestAnimationFrame(this.animFrame)
-    }
+      return {
+        to: [newLR, newUD],
+        dir: newLR || newUD || dir
+      }
+    })
   }
 
-  componentWillUnmount() {
-    // window.removeEventListener('keydown', this.keyDown.bind(this), true)
-    // window.removeEventListener('keyup', this.keyUp.bind(this), true)
+  keyUp = e => {
+    this.setState(({ to:[ lr, ud ] }) => {
+      const newLR = keyToLR[e.key]
+      const newUD = keyToUD[e.key]
+  
+      if (!newLR && !newUD) return
 
-    window.cancelAnimationFrame(this.animFrameRef)
+      return {
+        to: [newLR? null : lr, newUD? null : ud]
+      }
+    })
   }
 
   animFrame = time => {
@@ -35,32 +65,74 @@ class App extends Component {
 
     this.animFrameRef = window.requestAnimationFrame(this.animFrame)
   }
+  
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyDown)
+    window.addEventListener('keyup', this.keyUp)
 
-  center = [0,0]
+    if (!this.animFrameRef) {
+      this.animFrameRef = window.requestAnimationFrame(this.animFrame)
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.keyDown)
+    window.removeEventListener('keyup', this.keyUp)
+
+    window.cancelAnimationFrame(this.animFrameRef)
+  }
   
   render() {
-    const { center } = this
-    console.log(`window.devicePixelRatio ${window.devicePixelRatio}`)
+    const { xy, to:[ lr, ud ], dir, spr } = this.state
+
+    console.log(`to ${[lr,ud]}`)
+
+    const frm = lr || ud || dir || 'down'
+    
+    const [ f0, f1, f2, f3 ] = frames[frm]
     
     return (
       <Map
-        center={toLatLng(center)}
-        zoom={0}
         crs={crs}
+        center={toLatLng(xy)}
         
-        // keyboardPanDelta={0}
+        keyboard={false}
         // dragging={false}
+        
+        zoom={0}
         doubleClickZoom={false}
         scrollWheelZoom={false}
         touchZoom={false}
+
+        onClick={e => {
+          this.setState(({ spr }) => ({
+            spr: (spr+1)%24
+          }))
+        }}
       >
         <Ground />
 
         <DivIcon
-          position={toLatLng(center)}
+          position={toLatLng(xy)}
         >
-          <div className="me">
-            <img src={spritesPng} alt="" />
+          <div
+            className="me"
+            style={{
+              width: `${32}px`,
+              height: `${64}px`,
+            }}
+          >
+            <img
+              src={spritesPng} alt=""
+              style={{
+                transform: `
+                  translate(
+                    -${f3*32}px,
+                    -${spr*64}px
+                  )
+                `
+              }}
+            />
           </div>
         </DivIcon>
 
